23a455394fdc6c3e50d843386f944dbf8e90944b
Shamim Basha <shamimbasha13@gmail.com>
2026-02-24 20:35:15 +0530

    Added: Instructor Service class for with registration function of a new instructor with prevention of duplicate email and phone number

---
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/Service/InstructorService.java
@@ -0,0 +1,44 @@
+package lk.fat2fit.Fat2Fit.Service;
+
+import lk.fat2fit.Fat2Fit.DTO.Instructor.InstructorRegister;
+import lk.fat2fit.Fat2Fit.Entity.Instructor;
+import lk.fat2fit.Fat2Fit.Repository.InstructorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InstructorService {
+
+    private final InstructorRepository instructorRepository;
+
+    private Instructor instructorRegisterToInstructor(InstructorRegister instructorRegister){
+        return Instructor.builder()
+                .firstName(instructorRegister.getFirstName())
+                .lastName(instructorRegister.getLastName())
+                .phoneNumber(instructorRegister.getPhoneNumber())
+                .email(instructorRegister.getEmail())
+                .address(instructorRegister.getAddress())
+                .qualification(emptyToNull(instructorRegister.getQualification()))
+                .areasOfSpecialization(emptyToNull(instructorRegister.getAreasOfSpecialization()))
+                .build();
+    }
+
+    private String emptyToNull(String value) {
+        return (value == null || value.trim().isEmpty()) ? null : value;
+    }
+
+    public ResponseEntity<?> registerInstructor(InstructorRegister instructor){
+
+        if(instructorRepository.existsByEmailOrPhoneNumber(instructor.getEmail(), instructor.getPhoneNumber())){
+            ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Instructor with email: " + instructor.getEmail() +
+                            " or phone: " + instructor.getPhoneNumber() + " already exists");
+        }
+
+        instructorRepository.save(instructorRegisterToInstructor(instructor));
+        return ResponseEntity.ok().build();
+    }
+}
