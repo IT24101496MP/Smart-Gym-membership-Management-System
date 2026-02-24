457c9bc8de0197129929a00bde715dbf77f734d3
Shamim Basha <shamimbasha13@gmail.com>
2026-02-24 16:15:02 +0530

    Initialised: The SpringBoot Project Folder Structure with needed dependencies using Spring Initializr

---
+++ backend/src/test/java/lk/fat2fit/Fat2Fit/Fat2FitApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.fat2fit.Fat2Fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fat2FitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
