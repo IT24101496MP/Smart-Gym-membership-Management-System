006a5bdf22e267533995bd46d5f7f73801b12720
Minxpat <it24101496@my.sliit.lk>
2026-02-25 12:33:34 +0530

    Updated ClientRepository

--- backend/src/main/java/lk/fat2fit/Fat2Fit/Repository/ClientRepository.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/Repository/ClientRepository.java
@@ -1,1 +1,19 @@
+package lk.fat2fit.Fat2Fit.Repository;
 
+import lk.fat2fit.Fat2Fit.Entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+    boolean existsByEmail(String email);
+    boolean existsByMobileNumber(String mobileNumber);
+    boolean existsByEmailOrMobileNumber(String email, String mobileNumber);
+
+    Optional<Client> findByEmail(String email);
+    Optional<Client> findByMobileNumber(String mobileNumber);
+    List<Client> findByBloodGroup(String bloodGroup);
+
+}
