4a23b643c9f313b38a32e7ec3256959d66dac1cb
Minxpat <it24101496@my.sliit.lk>
2026-02-26 03:25:05 +0530

    Modified ClientRegister.java File

--- backend/src/main/java/lk/fat2fit/Fat2Fit/DTO/ClientRegister.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/DTO/ClientRegister.java
@@ -1,10 +1,14 @@
-package lk.fat2fit.Fat2Fit.DTO.Client;
+package lk.fat2fit.Fat2Fit.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientRegister {
 
     private String firstName;
