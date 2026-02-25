8ed39e12b1a2d672844c07b318c7f3fade11460f
Minxpat <it24101496@my.sliit.lk>
2026-02-25 12:53:47 +0530

    Updated ClientRegister

--- backend/src/main/java/lk/fat2fit/Fat2Fit/DTO/ClientRegister.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/DTO/ClientRegister.java
@@ -1,1 +1,26 @@
+package lk.fat2fit.Fat2Fit.DTO.Client;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ClientRegister {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String mobileNumber;
+    private String landPhone;
+    private String email;
+    private String address;
+
+    private String emergencyContactName;
+    private String emergencyContactRelationship;
+    private String emergencyContactNumber;
+
+    private String bloodGroup;
+    private byte[] image;
+    private byte[] digitalSignature;
+}
