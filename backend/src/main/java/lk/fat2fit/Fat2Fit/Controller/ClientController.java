cc80d711863eb8b81aeb204d94afd2a7563b2b95
Minxpat <it24101496@my.sliit.lk>
2026-02-25 12:58:54 +0530

    Updated ClientController

--- backend/src/main/java/lk/fat2fit/Fat2Fit/Controller/ClientController.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/Controller/ClientController.java
@@ -1,1 +1,20 @@
+package lk.fat2fit.Fat2Fit.Controller;
 
+import lk.fat2fit.Fat2Fit.DTO.Client.ClientRegister;
+import lk.fat2fit.Fat2Fit.Service.ClientService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/client")
+@AllArgsConstructor
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerClient(@RequestBody ClientRegister client){
+        return clientService.registerClient(client);
+    }
+}
