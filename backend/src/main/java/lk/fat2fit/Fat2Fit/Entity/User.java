3ddd63e4fb3f727f5fb967b49a2f124a01b8972a
Minxpat <it24101496@my.sliit.lk>
2026-02-27 14:09:37 +0530

    Added User.java

--- backend/src/main/java/lk/fat2fit/Fat2Fit/Entity/User.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/Entity/User.java
@@ -1,1 +1,46 @@
+package lk.fat2fit.Fat2Fit.Entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    public enum Role {
+        ADMIN,
+        INSTRUCTOR,
+        CLIENT
+    }
+
+    public enum Status {
+        PENDING,
+        APPROVED,
+        REJECTED
+    }
+}
