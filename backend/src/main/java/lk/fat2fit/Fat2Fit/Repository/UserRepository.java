5e85b307ec6e5c1650f4936a510956379ebd1fde
Minxpat <it24101496@my.sliit.lk>
2026-02-27 14:12:49 +0530

    Added UserRepository

--- backend/src/main/java/lk/fat2fit/Fat2Fit/Repository/UserRepository.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/Repository/UserRepository.java
@@ -1,1 +1,10 @@
+package lk.fat2fit.Fat2Fit.Repository;
 
+import lk.fat2fit.Fat2Fit.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
+}
