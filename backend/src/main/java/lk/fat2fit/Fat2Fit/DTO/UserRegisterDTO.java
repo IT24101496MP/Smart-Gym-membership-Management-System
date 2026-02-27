4aa0c741793c050c1119f086e3ce79fa01107468
Minxpat <it24101496@my.sliit.lk>
2026-02-27 14:15:24 +0530

    Added UserRegisterDTO.java

--- backend/src/main/java/lk/fat2fit/Fat2Fit/DTO/UserRegisterDTO.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/DTO/UserRegisterDTO.java
@@ -1,1 +1,10 @@
+package lk.fat2fit.Fat2Fit.DTO;
 
+import lombok.Data;
+
+@Data
+public class UserRegisterDTO {
+    private String username;
+    private String password;
+    private String role;
+}
