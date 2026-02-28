7c2ea41ef39fd4a11bc406412e5400795fb9cc85
Shamim Basha <shamimbasha13@gmail.com>
2026-02-28 21:18:06 +0530

    Created: Manage user controller and service to view and update the user details with role based access

---
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/DTO/Manage/UserDetailResponse.java
@@ -0,0 +1,36 @@
+package lk.fat2fit.Fat2Fit.DTO.Manage;
+
+import lk.fat2fit.Fat2Fit.Entity.Enum.Gender;
+import lk.fat2fit.Fat2Fit.Entity.Enum.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailResponse {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private LocalDate dateOfBirth;
+    private Gender gender;
+    private String email;
+    private String phoneNumber;
+    private String landPhone;
+    private String emergencyContactName;
+    private String emergencyContactRelationship;
+    private String emergencyContactNumber;
+    private String bloodGroup;
+    private String address;
+    private Role role;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
