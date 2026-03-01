35400de0c39c5b4e5a63719f819558cf4f3253d2
Shamim Basha <shamimbasha13@gmail.com>
2026-03-01 20:26:10 +0530

    Fixed: Merge Conflict

--- backend/src/main/java/lk/fat2fit/Fat2Fit/Service/InstructorService.java
+++ backend/src/main/java/lk/fat2fit/Fat2Fit/Service/InstructorService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder
 
 import lk.fat2fit.Fat2Fit.DTO.Instructor.InstructorEmploymentAssignment;
 import lk.fat2fit.Fat2Fit.DTO.Instructor.InstructorRegister;
