7ede477c4eb1d690793675b1d768438976b5581d
Minxpat <it24101496@my.sliit.lk>
2026-03-17 19:43:43 +0530

    Modified Cypress Files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,43 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginAsAdmin', () => {
+  cy.visit('http://localhost:5173/login');
+  cy.get('input[name="username"]').should('exist').type('admin@fat2fit.lk');
+  cy.get('input[name="password"]').should('exist').type('Admin@1234');
+  cy.get('button[type="submit"]').click();
+  cy.url().should('not.include', '/login');
+});
+
+Cypress.Commands.add('fillMembershipPlanForm', (plan) => {
+  if (plan.plan_name !== undefined && plan.plan_name !== '') {
+    cy.get('input[name="plan_name"]').should('exist').clear().type(plan.plan_name);
+  }
+  if (plan.description !== undefined) {
+    cy.get('textarea[name="description"]').should('exist').clear().type(plan.description);
+  }
+  if (plan.duration_days !== undefined && plan.duration_days !== '') {
+    cy.get('input[name="duration_days"]').should('exist').clear().type(plan.duration_days);
+  }
+  if (plan.monthly_price !== undefined && plan.monthly_price !== '') {
+    cy.get('input[name="monthly_price"]').should('exist').clear().type(plan.monthly_price);
+  }
+  if (plan.admission_fee !== undefined && plan.admission_fee !== '') {
+    cy.get('input[name="admission_fee"]').should('exist').clear().type(plan.admission_fee);
+  }
+  if (plan.maximum_members !== undefined && plan.maximum_members !== '') {
+    cy.get('input[name="maximum_members"]').should('exist').clear().type(plan.maximum_members);
+  }
+});
+
+Cypress.Commands.add('deactivatePlan', (planName) => {
+  cy.get('table tr').contains('td', planName)
+    .should('exist')
+    .parent() // get the row
+    .find('button.deactivate')
+    .should('exist')
+    .click();
+
+  cy.contains('Plan deactivated successfully').should('exist');
+  cy.get('table tr').contains('td', planName)
+    .parent()
+    .contains('td', 'INACTIVE')
+    .should('exist');
+});
