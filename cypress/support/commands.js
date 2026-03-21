2c3826b7a58591b6f2788b3f14fef3fb961fa93a
Minxpat <it24101496@my.sliit.lk>
2026-03-21 14:37:21 +0530

    Added required cypress files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,43 +1,24 @@
-Cypress.Commands.add('loginAsAdmin', () => {
-  cy.visit('http://localhost:5173/login');
-  cy.get('input[name="username"]').should('exist').type('admin@fat2fit.lk');
-  cy.get('input[name="password"]').should('exist').type('Admin@1234');
+Cypress.Commands.add('loginAsStaff', () => {
+  cy.visit('/login');
+  cy.fixture('users').then(users => {
+    cy.get('#identifier').type(users.admin.identifier);
+    cy.get('#password').type(users.admin.password);
+  });
   cy.get('button[type="submit"]').click();
   cy.url().should('not.include', '/login');
 });
 
-Cypress.Commands.add('fillMembershipPlanForm', (plan) => {
-  if (plan.plan_name !== undefined && plan.plan_name !== '') {
-    cy.get('input[name="plan_name"]').should('exist').clear().type(plan.plan_name);
-  }
-  if (plan.description !== undefined) {
-    cy.get('textarea[name="description"]').should('exist').clear().type(plan.description);
-  }
-  if (plan.duration_days !== undefined && plan.duration_days !== '') {
-    cy.get('input[name="duration_days"]').should('exist').clear().type(plan.duration_days);
-  }
-  if (plan.monthly_price !== undefined && plan.monthly_price !== '') {
-    cy.get('input[name="monthly_price"]').should('exist').clear().type(plan.monthly_price);
-  }
-  if (plan.admission_fee !== undefined && plan.admission_fee !== '') {
-    cy.get('input[name="admission_fee"]').should('exist').clear().type(plan.admission_fee);
-  }
-  if (plan.maximum_members !== undefined && plan.maximum_members !== '') {
-    cy.get('input[name="maximum_members"]').should('exist').clear().type(plan.maximum_members);
-  }
+// Open member profile
+Cypress.Commands.add('openMemberProfile', (memberId) => {
+  cy.visit(`/members/${memberId}`);
 });
 
-Cypress.Commands.add('deactivatePlan', (planName) => {
-  cy.get('table tr').contains('td', planName)
-    .should('exist')
-    .parent() // get the row
-    .find('button.deactivate')
-    .should('exist')
-    .click();
+// Click renew membership button
+Cypress.Commands.add('clickRenew', () => {
+  cy.contains('Renew Membership').click();
+});
 
-  cy.contains('Plan deactivated successfully').should('exist');
-  cy.get('table tr').contains('td', planName)
-    .parent()
-    .contains('td', 'INACTIVE')
-    .should('exist');
+// Confirm renewal
+Cypress.Commands.add('confirmRenew', () => {
+  cy.get('button').contains('Confirm').click();
 });
